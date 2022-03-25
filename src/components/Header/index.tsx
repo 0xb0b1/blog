8d2194fa9b72f39114791c515000df800c53c94a
Paulo Vicente <vcente82@gmail.com>
2022-03-25 09:50:37 -0300

    header with logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,9 @@
-export default function Header() {
-  // TODO
+import styles from './header.module.scss';
+
+export default function Header(): JSX.Element {
+  return (
+    <header className={styles.container}>
+      <img src="/Logo.svg" alt="Logo" />
+    </header>
+  );
 }
