3477825edb8c7c8c1afc739878984942affccda7
Paulo Vicente <vcente82@gmail.com>
2022-03-26 19:18:16 -0300

    ...

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,9 @@
+import { useState } from 'react';
+import { BsFillMoonFill, BsFillSunFill } from 'react-icons/bs';
 import styles from './header.module.scss';
 
 export default function Header(): JSX.Element {
+  const [darkTheme, setDarkTheme] = useState(false);
   return (
     <header className={styles.container}>
       <div className={styles.content}>
@@ -8,6 +11,10 @@ export default function Header(): JSX.Element {
         <h2>Paulo Vicente</h2>
         <p>Getting to know yourself by knowing the world</p>
       </div>
+
+      <button type="button" onClick={() => setDarkTheme(!darkTheme)}>
+        {darkTheme ? <BsFillMoonFill size={28} /> : <BsFillSunFill size={28} />}
+      </button>
     </header>
   );
 }
--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,42 +1,28 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
-import { useState } from 'react';
-import {
-  FiGithub,
-  FiInstagram,
-  FiLinkedin,
-  FiMoon,
-  FiTwitter,
-} from 'react-icons/fi';
-import { BsFillMoonFill, BsFillSunFill } from 'react-icons/bs';
+import { FiGithub, FiInstagram, FiLinkedin, FiTwitter } from 'react-icons/fi';
 import styles from './styles.module.scss';
 
 export const Sidebar = (): JSX.Element => {
-  const [darkTheme, setDarkTheme] = useState(false);
-
   return (
     <aside className={styles.aside}>
       <ul>
         <li className="">
           <FiGithub size={22} />
-          <span className="">Github</span>
+          {/* <span className="">Github</span> */}
         </li>
         <li>
           <FiTwitter size={22} />
-          <span>Twitter</span>
+          {/* <span>Twitter</span> */}
         </li>
         <li>
           <FiLinkedin size={22} />
-          <span>Linkedin</span>
+          {/* <span>Linkedin</span> */}
         </li>
         <li>
           <FiInstagram size={22} />
-          <span>Instagram</span>
+          {/* <span>Instagram</span> */}
         </li>
       </ul>
-
-      <button type="button" onClick={() => setDarkTheme(!darkTheme)}>
-        {darkTheme ? <BsFillMoonFill size={28} /> : <BsFillSunFill size={28} />}
-      </button>
     </aside>
   );
 };
